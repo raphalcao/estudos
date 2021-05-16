6ecb01ef00aaa11c7df7b27d9695a57546e223f5
Garbim <garbim7@gmail.com>
2021-05-16 11:50:57 -0300

    add home and layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <title>Home - @yield('title')</title>
+</head>
+
+<body>
+  <div class="container">
+    @yield('content')
+  </div>
+</body>
+
+</html>
